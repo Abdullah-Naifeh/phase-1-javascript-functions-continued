375b64f154c8ab0fe5e872c8c44fb0191f52d41c
Abdullah Naifeh <abdna59@gmail.com>
2022-10-24 14:36:33 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun( verb = "roller-skate"){
+    return `This Saturday, I want to ${verb}!`;
+}
+console.log(saturdayFun());
+
+function mondayWork(thisMonday = "go to the office"){
+    return `This Monday, I will ${thisMonday}.`
+}
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(flair = "*"){
+     return function (innerparam = "special"){
+        return `You are ${flair}${innerparam}${flair}!`
+    }
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
